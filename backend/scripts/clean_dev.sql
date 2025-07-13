06a92b1ff828d85f4b27d3a26558bae8eb96881d
Osmel Nuñez Alonso <rna@fullstep.com>
2025-07-13 19:26:51 +0200

    save changes

--- backend/scripts/clean_dev.sql
+++ backend/scripts/clean_dev.sql
@@ -1,4 +1,4 @@
-
-DELETE FROM expenses WHERE name LIKE 'Test Expense%';
-DELETE FROM incomes WHERE name LIKE 'Test Income%';
-DELETE FROM savings WHERE name LIKE 'Test Saving%';
+-- ELIMINAR DATOS DE PRUEBA (_devdata) ANTES DE CARGAR
+DELETE FROM savings WHERE name LIKE '%_devdata';
+DELETE FROM incomes WHERE name LIKE '%_devdata';
+DELETE FROM expenses WHERE name LIKE '%_devdata';
